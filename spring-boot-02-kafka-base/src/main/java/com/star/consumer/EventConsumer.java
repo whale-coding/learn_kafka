435659cb0d5bf2742d3e3c1792705bc7c824fa7d
whale-coding <njq3699@gmail.com>
2025-03-05 16:44:25 +0800

    手动ack确认

--- spring-boot-02-kafka-base/src/main/java/com/star/consumer/EventConsumer.java
+++ spring-boot-02-kafka-base/src/main/java/com/star/consumer/EventConsumer.java
@@ -4,6 +4,7 @@ import com.star.domain.User;
 import com.star.util.JSONUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -33,11 +34,11 @@ public class EventConsumer {
 
     // 通过ConsumerRecord<String, String> record接收消息（data）更加的方便
     // @KafkaListener(topics = { "helloTopic"}, groupId = "helloGroup")
-    public void onEvent2(ConsumerRecord<String, String> record) {
+    public void onEvent1(ConsumerRecord<String, String> record) {
         System.out.println("读取到的事件1：" + record.toString());
     }
 
-    @KafkaListener(topics = { "helloTopic"}, groupId = "helloGroup")
+    // @KafkaListener(topics = { "helloTopic"}, groupId = "helloGroup")
     public void onEvent2(String userJSON,
                          @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
                          @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
@@ -45,6 +46,40 @@ public class EventConsumer {
         User user = JSONUtils.toBean(userJSON, User.class);  // json转对象
         System.out.println("读取到的事件2：" + user + ", topic : " + topic + ", partition : " + partition);
         System.out.println("读取到的事件2：" + record.toString());
+    }
+
+    // 使用占位符读取自定义配置的topic名称等
+    // @KafkaListener(topics = { "${kafka.topic.name}"}, groupId = "${kafka.consumer.group}")
+    public void onEvent3(String userJSON,
+                         @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+                         @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
+                         @Payload ConsumerRecord<String, String> record) {
+        User user = JSONUtils.toBean(userJSON, User.class);
+        System.out.println("读取到的事件3：" + user + ", topic : " + topic + ", partition : " + partition);
+        System.out.println("读取到的事件3：" + record.toString());
+    }
+
+    // Acknowledgment ack参数，开启手动消息确认模式
+    @KafkaListener(topics = { "${kafka.topic.name}"}, groupId = "${kafka.consumer.group}")
+    public void onEvent4(String userJSON,
+                         @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+                         @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
+                         @Payload ConsumerRecord<String, String> record,
+                         Acknowledgment ack) {
+
+        try {
+            //收到消息后，处理业务
+            User user = JSONUtils.toBean(userJSON, User.class);
+            System.out.println("读取到的事件4：" + user + ", topic : " + topic + ", partition : " + partition);
+            System.out.println("读取到的事件4：" + record.toString());
+
+            // int a = 10 / 0;
+
+            // 业务处理完成，给kafka服务器确认（手动确认，默认是kafka自动确认）
+            ack.acknowledge(); // 手动确认消息，就是告诉kafka服务器，该消息我已经收到了，默认情况下kafka是自动确认
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
